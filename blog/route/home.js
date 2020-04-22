9207a0ba4f1c7650ddf689ba588a1f3fe9de7815
lichangying <285777553@qq.com>
2020-04-22 17:33:58 +0800

    jx4

--- blog/route/home.js
+++ blog/route/home.js
@@ -3,9 +3,10 @@ const express = require('express');
 // 创建博客展示页面路由
 const home = express.Router();
 
-home.get('/', (req, res) => {
-	res.send('欢迎来到博客首页')
-});
+home.get('/', require('./home/index'))
+
+home.get('/article',require('./home/article'))
+
 
 // 将路由对象做为模块成员进行导出
 module.exports = home;
---
+++ blog/route/home/index.js
@@ -0,0 +1,6 @@
+module.exports = (req, res) => {
+    // res.send('欢迎来到博客首页面')
+
+    res.render('home/default.art')
+
+}
